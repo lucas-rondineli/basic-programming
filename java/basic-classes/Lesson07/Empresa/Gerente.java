7b4f71fcfab79ca651ecd23158b9ea10fc683be0
Lucas Rondineli <lucas.rondinele93@gmail.com>
2023-05-22 10:49:51 -0300

    First step on fixing the folders names.

--- java/basic-classes/Lesson07/Empresa/Gerente.java
+++
@@ -1,8 +0,0 @@
-package lesson07.empresa;
-
-public class Gerente extends Funcionario {
-    public Gerente(String nome, double salario, int id) {
-        super(nome, salario, id);
-        this.salario *= 1.2;
-    }
-}
